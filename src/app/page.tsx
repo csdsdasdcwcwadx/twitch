1242546dacee6abfcf774abab901ccccc4e0ae71
csdsdasdcwcwadx <87317853+csdsdasdcwcwadx@users.noreply.github.com>
2025-01-07 21:59:47 +0800

    登入邏輯更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import logoSrc from '@/icon/logo.jpg';
-import Link from "next/link";
+import LoginButton from "@/components/pages/app/button";
 
 export default function Home() {
   return (
@@ -8,13 +8,7 @@ export default function Home() {
       <figure className="m-auto relative pc:h-[80vh] aspect-[1]">
         <Image priority className="animate-moving w-full h-full object-cover max-w-xl absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2" src={logoSrc} alt="logo"/>
       </figure>
-      <Link href='/login'>
-        <button 
-          className="bg-[#f1b600] py-5 px-10 pl-10 font-bold text-background tracking-widest text-[17px] rounded-[20px] mx-auto block"
-        >
-          開始
-        </button>
-      </Link>
+      <LoginButton/>
     </main>
   );
 }
