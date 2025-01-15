05a3f0f0bca3fe90a2a92d744b13e60338569ba5
csdsdasdcwcwadx <87317853+csdsdasdcwcwadx@users.noreply.github.com>
2025-01-16 00:49:55 +0800

    新增簽到表

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -1,16 +1,39 @@
 'use client';
 
-import { getchecks } from "@/utils/api"
+import { getchecks, setUserCheck } from "@/utils/api"
 import { useEffect, useState } from "react";
+import { getMonthCalendar } from "@/utils/util";
+
+const today = new Date();
+
+type Check = {
+    id: string;
+    passcode: string;
+    created_at: string;
+    userChecks: UserCheck[];
+}
+
+type UserCheck = {
+    user_id: string;
+    check_id: string;
+    checked: boolean;
+    created_at: string;
+}
+
+type I_CheckPage = {
+    getChecks: Check[];
+}
 
 export default function Check () {
+    const [currData, setCurrData] = useState<I_CheckPage>({
+        getChecks: [],
+    });
+
     useEffect(() => {
         (async function () {
-            const urlParams = new URLSearchParams(window.location.href.split('?')[1]);
-            const userID = urlParams.get("userID");
             try {
-                const result = await getchecks(userID!);
-                console.log(result);
+                const result = await getchecks();
+                setCurrData(result);
             } catch(e) {
                 console.log(e)
             }
@@ -19,13 +42,46 @@ export default function Check () {
 
     return (
         <main>
-            <button>
-                check
-            </button>
             <table width="500">
                 <caption>簽到表</caption>
                 <tbody>
-                    <tr></tr>
+                    {
+                        getMonthCalendar(today.getFullYear(), today.getMonth() + 1).map((week, index) => {
+                            return (
+                                <tr key={index}>
+                                    {
+                                        week.map((day, weekIndex) => {
+                                            const checks = currData.getChecks.find(check => {
+                                                const currentDay = new Date(Number(check.created_at)).getDate();
+                                                return `${currentDay}` === day;
+                                            })
+                                            return (
+                                                <td key={`${weekIndex}${index}`} className="text-right cursor-pointer py-2 align-top h-20 relative">
+                                                    <div>{day}</div>
+                                                    { 
+                                                        checks && <div onClick={async () => {
+                                                            await setUserCheck(checks.id, true);
+                                                        }} className="absolute text-xs right-0">直播</div> 
+                                                    }
+                                                    {
+                                                        checks && (checks.userChecks.length ? checks.userChecks.map((check, index) => {
+                                                            return (
+                                                                <div key={index} className="absolute text-xs right-0 top-12">
+                                                                    {
+                                                                        check.checked ? '已簽到' : '尚未簽到'
+                                                                    }
+                                                                </div>
+                                                            )
+                                                        }) : <div className="absolute text-xs right-0 top-12">尚未簽到</div>)
+                                                    }
+                                                </td>
+                                            )
+                                        })
+                                    }
+                                </tr>
+                            )
+                        })
+                    }
                 </tbody>
             </table>
         </main>
