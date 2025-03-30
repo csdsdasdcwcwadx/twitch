8386531aa93610d68f70975317025ee48a6cf950
csdsdasdcwcwadx <87317853+csdsdasdcwcwadx@users.noreply.github.com>
2025-03-31 06:18:32 +0800

    修正環境變數名稱

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,6 +1,6 @@
 import { E_Item_Types } from "./interface";
 
-export const domainEnv = process.env.ENV === 'prod' ? 'http://ec2-54-253-97-210.ap-southeast-2.compute.amazonaws.com' : '/api';
+export const domainEnv = process.env.NEXT_PUBLIC_ENV === 'prod' ? 'http://ec2-54-253-97-210.ap-southeast-2.compute.amazonaws.com' : '/api';
 
 export const twitchIconDomain = "https://static-cdn.jtvnw.net";
 export const ImagePath = `${domainEnv}/twitch/item/images/`;
