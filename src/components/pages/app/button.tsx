a6609330ea9436afc7343c47287964ef8e7c9cf1
csdsdasdcwcwadx <87317853+csdsdasdcwcwadx@users.noreply.github.com>
2025-03-31 00:08:56 +0800

    修正請求路徑

--- src/components/pages/app/button.tsx
+++ src/components/pages/app/button.tsx
@@ -6,8 +6,8 @@ function LoginButton () {
         <button
             className="bg-[#f1b600] py-5 px-10 pl-10 font-bold text-background tracking-widest text-[17px] rounded-[20px] mx-auto block"
             onClick={() => {
-                const clientId = "lfirafgpkknrzxg6412jb9y8u5nd34";
-                const redirectUri = `${window.location.origin}/twitch/member/login`;
+                const clientId = process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID;
+                const redirectUri = `${process.env.NEXT_PUBLIC_SERVER_HOST}/twitch/member/login`;
                 const scope = "user:read:email";
                 const force_verify = true;
                 const login_type = 'login';
