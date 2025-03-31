bf6de2f126e01208fbb3db4cdfa6cb8489853264
csdsdasdcwcwadx <87317853+csdsdasdcwcwadx@users.noreply.github.com>
2025-03-31 22:07:04 +0800

    修正遠端路徑

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,6 +1,6 @@
 import { E_Item_Types } from "./interface";
 
-export const domainEnv = process.env.NEXT_PUBLIC_ENV === 'prod' ? 'http://ec2-54-253-97-210.ap-southeast-2.compute.amazonaws.com' : '/api';
+export const domainEnv = process.env.NEXT_PUBLIC_ENV === 'prod' ? process.env.NEXT_PUBLIC_SERVER_HOST : '/api';
 
 export const twitchIconDomain = "https://static-cdn.jtvnw.net";
 export const ImagePath = `${domainEnv}/twitch/item/images/`;
