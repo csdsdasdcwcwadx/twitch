65848cf89afca67c0754aa2030d24a58a0ebd0a0
Jack.Tsai <Jack.Tsai@acti.com>
2025-04-11 17:51:19 +0800

    圖片存取修正

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,8 +1,9 @@
 import { E_Item_Types } from "./interface";
 
-export const domainEnv = `${process.env.NEXT_PUBLIC_SERVER_HOST}${process.env.NEXT_PUBLIC_ENV === "prod" ? "/twitch" : "/api"}`;
+export const redirectPath = process.env.NEXT_PUBLIC_ENV === "prod" ? "/twitch" : "/api";
+export const domainEnv = `${process.env.NEXT_PUBLIC_SERVER_HOST}${redirectPath}`;
 
 export const twitchIconDomain = "https://static-cdn.jtvnw.net";
-export const ImagePath = `${domainEnv}/item/images/`;
+export const ImagePath = `${redirectPath}/item/images/`;
 
 export const ItemTypes = Object.values(E_Item_Types);
