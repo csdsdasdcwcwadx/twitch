9da34672c3a89894140d2d2efefa3e08b1babb05
csdsdasdcwcwadx <87317853+csdsdasdcwcwadx@users.noreply.github.com>
2025-07-20 17:10:05 +0800

    新增文字

--- src/components/pages/check/Inform.tsx
+++ src/components/pages/check/Inform.tsx
@@ -9,6 +9,7 @@ function Inform () {
                 <Image src={avatarSrc} alt="avatar"/>
             </figure>
             <p className="w-[250px] mobile:w-auto mobile:mt-5 text:foreground">
+                <span className="block text-xl text-center font-bold mb-5">簽到說明</span>
                 <span className="block text-center">在開放簽到欄位中點擊簽到並輸入通關密語即可完成簽到 </span>
                 <span className="block text-center mt-3 mobile:mt-1">成功簽到可以領取道具 {'<'}天選之人{'>'} 卡牌</span> 
                 <span className="block text-center mt-3 mobile:mt-1">累積30張兌換神秘禮物 !</span>
