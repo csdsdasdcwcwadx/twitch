82b5dfcacd255e0a51dad7bcb38262b77f19a7a2
csdsdasdcwcwadx <87317853+csdsdasdcwcwadx@users.noreply.github.com>
2025-08-26 21:31:09 +0800

    將messages 正規化列入queue

--- src/components/pages/alert/Display.tsx
+++ src/components/pages/alert/Display.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { memo, useEffect, useState } from "react"
+import { memo, useEffect, useState, useRef} from "react"
 import { initOpaySocket } from "@/utils/util";
 import Image from "next/image";
 import headIcon from "@/icon/head.gif";
@@ -10,30 +10,59 @@ interface I_WSMessage {
     DonateNickName: string;
     DonateAmount: string;
     DonateMsg: string;
+    sound?: string;
 }
 
-let timer: NodeJS.Timeout | null = null;
 function Display() {
     const [message, setMessage] = useState<I_WSMessage | null>(null);
+    const [messageQueue, setMessageQueue] = useState<I_WSMessage[]>([]);
+    const timerRef = useRef<NodeJS.Timeout | null>(null);
 
     useEffect(() => {
+        const event = {
+            "data": {
+                "lstDonate": [
+                    {
+                        "donateid": "14012074",
+                        "name": "XXX",
+                        "amount": 100,
+                        "msg": "這是一筆贊助測試～"
+                    }
+                ],
+                "settings": {
+                    "BgColor": "#FFFFFF",
+                    "FontAnimate": "Wobble",
+                    "MsgTemplate": "&#22909;&#38283;&#21235; &#35613;&#35613; {name} wow {amount} !!",
+                    "AlertSound": "/Upload/Broadcaster/2205931/alertSound_20210626122807.mp3",
+                    "AlertSec": 15,
+                    "AlertStyle": 0,
+                    "TTSStatus": 1,
+                    "TTSVolume": 34,
+                    "AlertSoundVolume": 31,
+                    "FontSize": 32
+                }
+            }
+        }
         const alertSocket = io(`http://localhost:4000/socket/alert`); // 需要再修改
         const opaySocket = initOpaySocket(
             "https://socket.opay.tw/web/live/C4DB659FF82BAB591BA43075C2A5B0D7",
             "/web/live/C4DB659FF82BAB591BA43075C2A5B0D7",
         );
-        opaySocket.on("notify", (message) => {
-            console.log(message);
+        opaySocket.on("notify", () => {
+            for (let i = 0; i < event.data.lstDonate.length; i++) {
+                const currentContent = event.data.lstDonate[i];
+                const message = {
+                    DonateNickName: currentContent.name,
+                    DonateAmount: currentContent.amount.toString(),
+                    DonateMsg: currentContent.msg,
+                    sound: event.data.settings.AlertSound,
+                }
+                setMessageQueue(queue => [...queue, message]);
+            }
         });
-        alertSocket.on("notify", (data) => {
-        //     const message = JSON.parse(event.data) as I_WSMessage;
-            if (timer) clearTimeout(timer);
-
-        //     setMessage(message)
-            timer = setTimeout(() => {
-                setMessage(null);
-            }, 10000)
-            console.log("🚨 Alert:", data);
+        alertSocket.on("notify", (event) => {
+            const message = JSON.parse(event.data) as I_WSMessage;
+            setMessageQueue(queue => [...queue, message]);
         });
 
         return () => {
@@ -41,6 +70,24 @@ function Display() {
             alertSocket.disconnect();
         }
     }, [])
+
+    useEffect(() => {
+        if (!timerRef.current && messageQueue.length > 0) {
+            timerRef.current = setTimeout(() => {
+                setMessageQueue(prevQueue => {
+                    if (prevQueue.length === 0) return prevQueue;
+                    const [first, ...rest] = prevQueue;
+                    setMessage(first);
+                    timerRef.current = null;
+                    return rest;
+                })
+            }, 5000);
+        }
+
+        return () => {
+            if (timerRef.current) clearTimeout(timerRef.current);
+        }
+    }, [messageQueue])
     
     return (
         <div id="divAlert" className="font-[Open_Sans] relative text-center">
